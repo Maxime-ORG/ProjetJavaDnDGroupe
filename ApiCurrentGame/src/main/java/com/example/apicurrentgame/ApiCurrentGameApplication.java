e4c0df1f4bbc8c4005f92623f072434e359be9b3
Maxime-ORG <143593084+Maxime-ORG@users.noreply.github.com>
2023-12-04 09:29:05 +0100

    first commit

---
+++ ApiCurrentGame/src/main/java/com/example/apicurrentgame/ApiCurrentGameApplication.java
@@ -0,0 +1,13 @@
+package com.example.apicurrentgame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCurrentGameApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiCurrentGameApplication.class, args);
+    }
+
+}
---
+++ ApiCurrentGame/src/test/java/com/example/apicurrentgame/ApiCurrentGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apicurrentgame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCurrentGameApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
